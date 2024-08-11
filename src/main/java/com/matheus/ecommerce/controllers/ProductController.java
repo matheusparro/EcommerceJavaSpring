c6b5e0408df03ad3ad8b7072101b858dc9c89be7
Matheus Parro <mathparro@gmail.com>
2024-08-11 17:29:50 -0300

    feat: PUT new product

--- src/main/java/com/matheus/ecommerce/controllers/ProductController.java
+++ src/main/java/com/matheus/ecommerce/controllers/ProductController.java
@@ -34,9 +34,15 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
-        dto = service.insert(dto);
+        ProductDTO dtoInserted = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(dto.getId()).toUri();
-        return ResponseEntity.created(uri).body(dto);
+                .buildAndExpand(dtoInserted.getId()).toUri();
+        return ResponseEntity.created(uri).body(dtoInserted);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok().body(dto);
     }
 }
